2c8b30b1160129586e8439d7f6cd76bb62d8b4c7
Michael Gfeller <mgfeller@mgfeller.net>
2020-09-27 00:00:16 +0200

    moved Viper config provider to common library
    Signed-off-by: Michael Gfeller <mgfeller@mgfeller.net>

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,12 +3,13 @@ package config
 import (
 	"github.com/mgfeller/common-adapter-library/adapter"
 	"github.com/mgfeller/common-adapter-library/config"
+	"github.com/mgfeller/common-adapter-library/config_provider"
 )
 
 func New(name string, serverConfig map[string]string, meshConfig map[string]string, providerConfig map[string]string, operations adapter.Operations) (config.Handler, error) {
 	switch name {
 	case "viper":
-		return NewViper(serverConfig, meshConfig, providerConfig, operations)
+		return config_provider.NewViper(serverConfig, meshConfig, providerConfig, operations)
 	}
 	return nil, config.ErrEmptyConfig
 }
