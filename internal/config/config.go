fc6c0818a38e58f9839e4ead482cee73355478c8
Michael Gfeller <mgfeller@mgfeller.net>
2020-09-29 22:47:50 +0200

    adjusted config provider package name
    Signed-off-by: Michael Gfeller <mgfeller@mgfeller.net>

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,13 +3,13 @@ package config
 import (
 	"github.com/mgfeller/common-adapter-library/adapter"
 	"github.com/mgfeller/common-adapter-library/config"
-	"github.com/mgfeller/common-adapter-library/config_provider"
+	"github.com/mgfeller/common-adapter-library/configprovider"
 )
 
 func New(name string, serverConfig map[string]string, meshSpec map[string]string, meshInstance map[string]string, providerConfig map[string]string, operations adapter.Operations) (config.Handler, error) {
 	switch name {
 	case "viper":
-		return config_provider.NewViper(serverConfig, meshSpec, meshInstance, providerConfig, operations)
+		return configprovider.NewViper(serverConfig, meshSpec, meshInstance, providerConfig, operations)
 	}
 	return nil, config.ErrEmptyConfig
 }
