289dc714de75f884feb3ffe900ae5cb1ba73b416
ashish <ashishjaitiwari15112000@gmail.com>
2021-11-05 13:43:49 +0530

    few changes
    Signed-off-by: ashish <ashishjaitiwari15112000@gmail.com>

--- internal/config/releases.go
+++ internal/config/releases.go
@@ -59,7 +59,6 @@ func GetLatestReleases(releases uint) ([]*Release, error) {
 }
 
 // GetFileNames takes the url of a github repo and the path to a directory. Then returns all the filenames from that directory
-<<<<<<< HEAD
 func GetFileNames(owner string, repo string, path string) ([]string, error) {
 	g := walker.NewGit()
 	var filenames []string
@@ -68,78 +67,4 @@ func GetFileNames(owner string, repo string, path string) ([]string, error) {
 		return nil
 	}).Walk()
 	return filenames, err
-=======
-func GetFileNames(url string, path string) ([]string, error) {
-	res, err := http.Get(url + "/commits") //url="https://api.github.com/repos/hashicorp/consul-k8s"
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-	content, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	var r []rootdir
-	err = json.Unmarshal(content, &r)
-	if err != nil {
-		return nil, err
-	}
-	shaurl := r[0].Commit.Tree.URL
-	bpath := strings.Split(path, "/")
-	ans, err := _getname(shaurl, bpath)
-	if err == nil {
-		return ans, nil
-	}
-	return ans, ErrGetManifestNames(err)
-}
-
-func _getname(shaURL string, bpath []string) ([]string, error) {
-	var ans []string
-	res, err := http.Get(shaURL) // #nosec
-	if err != nil {
-		return []string{}, err
-	}
-	defer res.Body.Close()
-	var t treeslice
-	content, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return []string{}, err
-	}
-	err = json.Unmarshal(content, &t)
-	if err != nil {
-		return nil, err
-	}
-	if len(bpath) != 0 {
-		dirName := bpath[0]
-		bpath = bpath[1:]
-
-		for _, c := range t.Tree {
-			var (
-				url string
-				ok  bool
-			)
-			if url, ok = c["url"].(string); !ok {
-				return nil, errors.New("invalid URL field")
-			}
-			if c["path"] == dirName {
-				tempans, err := _getname(url, bpath)
-				return tempans, err
-			}
-		}
-		return []string{}, err
-	}
-
-	//base case
-	for _, c := range t.Tree {
-		var (
-			path string
-			ok   bool
-		)
-		if path, ok = c["path"].(string); !ok {
-			return nil, errors.New("invalid path field")
-		}
-		ans = append(ans, path)
-	}
-	return ans, nil
->>>>>>> upstream/master
 }
